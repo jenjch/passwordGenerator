9a7695d4d54b390d89463404b2a0d5b021bc165a
Jen C <jenjch8@gmail.com>
2019-11-07 19:04:45 -0600

    created html, javascript files with a prompt and a few confirms

---
+++ script.js
@@ -0,0 +1,12 @@
+var lowerChar = "abcdefghijklmnopqrstuvwxyz"
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var num = "0123456789"
+var sym = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~\""
+
+var charLength = prompt ("How many characters would you like your password to contain?")
+
+var confirmSym = confirm ("Click OK to confirm including special characters.")
+var confirmNum = confirm ("Click OK to confirm including numbers.")
+var confirmLower = confirm ("Click OK to confirm including lowercase characters.")
+var confirmUpper = confirm ("Click OK to confirm including uppercase characters.")
+
