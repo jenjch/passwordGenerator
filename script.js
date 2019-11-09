e90066d2cf6d4fb8ce862f1090cd0f32e84ee6bc
Jen C <jenjch8@gmail.com>
2019-11-09 14:02:15 -0600

    tried to output password to page with function to concatenate password based on character types confimed

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var symbol = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
 
+var outputPwd = document.getElementById ("outputPwd")
+
 // Can only be numbers. Else message "You must enter a number!".
 // Has to be between 8 and 128 characters. Else message "Password length must be between 128 characters."
 function checkNum (num){
@@ -17,24 +19,50 @@ document.querySelector(".generate-pw").onclick = function(event) {
 //   console.log(convertedNumber);
   if (!checkNum (convertedNumber)) {
     alert("You must enter a number!");
-    charLength = prompt(
-      "How many characters would you like your password to contain?"
-    );
+    // charLength = prompt(
+    //   "How many characters would you like your password to contain?"
+    // );
     } else if (charLength < 8 || charLength > 128) {
     alert("Password must be between 8 and 128 characters!");
-    charLength = prompt(
-      "How many characters would you like your password to contain?"
-    );
+    // charLength = prompt(
+    //   "How many characters would you like your password to contain?"
+    // );
     } else {
     var confirmSym = confirm("Click OK to confirm including special characters.");
     var confirmNum = confirm("Click OK to confirm including numbers.");
     var confirmLower = confirm("Click OK to confirm including lowercase characters.");
     var confirmUpper = confirm("Click OK to confirm including uppercase characters.");
-  }
+//  if at least one is true, then generate password?
+//  Else message "Must select at least once character type."
+        if (confirmSym || confirmNum || confirmLower || confirmUpper) {
+            var characters = '';
+            (confirmSym) ? characters += symbol : '';
+            (confirmNum) ? characters += number : '';
+            (confirmLower) ? characters += lowerChar : '';
+            (confirmUpper) ? characters += upperChar : '';
+            outputPwd.value = password(charLength.value, characters);
+        function password (l,characters) {
+            var pwd = '';
+            for (var i=0 ; i<l; i++) {
+                pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+                }
+                return pwd;
+            }
+            console.log(password(8,symbol));
+            
+        };
+    };
 };
-// if at least one is true, then generate password. Need to have Or's?
-// Else message "Must select at least once character type."
 
+// function password (l,characters) {
+//     var pwd = '';
+//     for (var i=0 ; i<l; i++) {
+//         pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+//     }
+//     return pwd;
+// }
+
+// console.log(password(8,upperChar));
 
 // Other Notes
 // How to continue to prompt the appropriate error message and "How many characters wuld you like your password to contain?" if the entry continues to be incorrect?
@@ -45,3 +73,4 @@ document.querySelector(".generate-pw").onclick = function(event) {
 // Reset if click "Generate Password" again?
 // Bonus: have copy to clipboard option
 // Bonus: display previously generated password
+// regex to check if prompt is a number
