4745ce0364f57459e9fdcd314d1dead22d3c6d87
Jen C <jenjch8@gmail.com>
2019-11-09 19:04:42 -0600

    fixed password output to page and added code to re-run generate password function if input specs not met

--- script.js
+++ script.js
@@ -5,72 +5,63 @@ var symbol = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
 
 var outputPwd = document.getElementById ("outputPwd")
 
-// Can only be numbers. Else message "You must enter a number!".
-// Has to be between 8 and 128 characters. Else message "Password length must be between 128 characters."
 function checkNum (num){
     return /^[0-9.,]+$/.test(num)
 }
-document.querySelector(".generate-pw").onclick = function(event) {
+document.querySelector(".generate-pw").addEventListener("click", generatePassword);
+
+function generatePassword(event) {
   var charLength = prompt(
     "How many characters would you like your password to contain?"
   );
-//   console.log(charLength);
-  var convertedNumber = parseInt(charLength);
-//   console.log(convertedNumber);
-  if (!checkNum (convertedNumber)) {
+var convertedNumber = parseInt(charLength);
+
+if (!checkNum (convertedNumber)) {
     alert("You must enter a number!");
-    // charLength = prompt(
-    //   "How many characters would you like your password to contain?"
-    // );
-    } else if (charLength < 8 || charLength > 128) {
-    alert("Password must be between 8 and 128 characters!");
-    // charLength = prompt(
-    //   "How many characters would you like your password to contain?"
-    // );
-    } else {
+    
+    generatePassword();
+    return;
+
+} else if (charLength < 8 || charLength > 128) {
+    alert("Your password must be between 8 and 128 characters!");
+    generatePassword();
+    return;
+
+} else {
     var confirmSym = confirm("Click OK to confirm including special characters.");
     var confirmNum = confirm("Click OK to confirm including numbers.");
     var confirmLower = confirm("Click OK to confirm including lowercase characters.");
     var confirmUpper = confirm("Click OK to confirm including uppercase characters.");
-//  if at least one is true, then generate password?
-//  Else message "Must select at least once character type."
+
         if (confirmSym || confirmNum || confirmLower || confirmUpper) {
             var characters = '';
-            (confirmSym) ? characters += symbol : '';
-            (confirmNum) ? characters += number : '';
-            (confirmLower) ? characters += lowerChar : '';
-            (confirmUpper) ? characters += upperChar : '';
-            outputPwd.value = password(charLength.value, characters);
-        function password (l,characters) {
-            var pwd = '';
-            for (var i=0 ; i<l; i++) {
-                pwd += characters.charAt(Math.floor(Math.random() * characters.length));
-                }
-                return pwd;
-            }
-            console.log(password(8,symbol));
-            
-        };
+            if (confirmSym) characters += symbol;
+            if (confirmNum) characters += number;
+            if (confirmLower) characters += lowerChar;
+            if (confirmUpper) characters += upperChar;
+            var password = getPassword(convertedNumber, characters);
+    
+            outputPwd.value = getPassword(convertedNumber, characters);
+
+        } else {
+            alert("You must select at least one character type!");
+            generatePassword();
+            return;
+        }
     };
 };
 
-// function password (l,characters) {
-//     var pwd = '';
-//     for (var i=0 ; i<l; i++) {
-//         pwd += characters.charAt(Math.floor(Math.random() * characters.length));
-//     }
-//     return pwd;
-// }
+function getPassword (l, characters) {
+    var pwd = '';
+    for (var i=0; i<l; i++) {
+        pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pwd;
+}
 
 // console.log(password(8,upperChar));
 
 // Other Notes
-// How to continue to prompt the appropriate error message and "How many characters wuld you like your password to contain?" if the entry continues to be incorrect?
-// Use Math.floor, Math.random, charAt to randomize char/num/sym
-// Do something to concatenate the values to a complete password
-// First the "Generate Password" button needs to be clicked before running the prompts and the confirms
-// Display password output on page
-// Reset if click "Generate Password" again?
 // Bonus: have copy to clipboard option
-// Bonus: display previously generated password
+// See if you can display previously generated password
 // regex to check if prompt is a number
